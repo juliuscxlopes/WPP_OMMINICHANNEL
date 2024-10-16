75cea2e13fd618b3edce7cc6acd8e353d93f18ed
Julius Lopes <juliuscxlopes@gmail.com>
2024-10-16 18:55:04 -0300

    acertos

--- src/routes/webhookRoutes.js
+++ src/routes/webhookRoutes.js
@@ -2,14 +2,13 @@
 const express = require('express');
 const router = express.Router();
 const { webhookController } = require('../controllers/webhookController');
-const { supportService } = require('../Services/supportService/supportService');
 const verificationController = require('../controllers/verificationController');
 const messageProcessor = require('../middleware/messageProcessor');
-const statusProcessor = require('../middleware/statusProcessor');
+
 
 
 // Middleware de processamento básico
-router.post('/', messageProcessor, statusProcessor, webhookController);
+router.post('/', messageProcessor, webhookController);
 
 // Rota para verificação do webhook
 router.get('/', verificationController.verifyWebhook);
